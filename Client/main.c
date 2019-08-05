8b4d48594628f7560272f930c484616509c5b0d9
thd <sherbakov_n@mail.ru>
2019-08-05 22:24:07 +0300

    Added logging and done some polishing.

--- Client/main.c
+++ Client/main.c
@@ -11,25 +11,66 @@
 #define SERVER_ADDRESS "127.0.0.1"
 #define MAC_LENGTH 32
 #define MESSAGE_LENGTH 256
+#define FILE_NAME "tst.txt"
+#define TRUE 1
+#define FALSE 0
 
-int main(void)
+void log_message(FILE *log_file, char *msg, int bool_error)
 {
+    if(!log_file)
+        printf("%s\n", msg);
+    else
+    {
+        fprintf(log_file, "%s", msg);
+        if(bool_error)
+            fclose(log_file);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+
+    if( argc < 2 || argc > 3 )
+    {
+        printf("Usage: ./Client <parameter>\n"
+               "-c Console logging\n"
+               "-f <log_file_name> File logging\n");
+        return -1;
+    }
+
 
     if(sodium_init() < 0)
     {
-        perror("No sodium");
+        perror("No sodium\n");
         return -1;
     }
-    FILE* fp;
+
+    FILE* log_file = NULL;
+
+    if ( (strcmp(argv[1], "-f") == 0) && (argc == 3))
+    {
+        if ( (log_file = fopen(argv[2], "w")) == NULL)
+        {
+            perror("Couldn't create file\n");
+            return -1;
+        }
+    }
+    else if( (strcmp(argv[1], "-c") != 0) || (argc != 2))
+    {
+        printf("Wrong parameters. Check usage with ./Client\n");
+        return -1;
+    }
+
+    FILE* read_fp;
     struct sockaddr_in serv_addr, user_addr;
-    int user_fd = 0, key_len = 0;
+    int user_fd = 0;
     unsigned int       len = sizeof(struct sockaddr);
     unsigned char      message_buf[MESSAGE_LENGTH];
     unsigned char      cipher_msg[MESSAGE_LENGTH + crypto_aead_xchacha20poly1305_ietf_ABYTES];
     unsigned char      server_payload[4] = {0x00,0x00,0x00,0x01};
     unsigned char      user_payload[4] = {0x00,0x00,0x00,0x02};
     unsigned char      ip_buf[16];              //Length is 16 because maximum IP length with dots is 15 + '\0'
-    unsigned char      ip_payload[9];            //Length is 9 because 4 digits of IP + 4 digits of payload + '\0'
+    unsigned char      ip_payload[9];           //Length is 9 because 4 digits of IP + 4 digits of payload + '\0'
     unsigned char*     ip_pay_ptr = ip_payload;
     unsigned char      MAC_CLIENT[MAC_LENGTH];
     unsigned char      MAC_SERVER[MAC_LENGTH];
@@ -51,14 +92,18 @@ int main(void)
 
     if((user_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("Error while creating socket");
+        char* error = "Error while creating socket";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
 
+    log_message(log_file, "Socket created", FALSE);
+
     if((inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr)) <= 0)
     {
-        perror("Address error\n");
+        char* error = "Server address error";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
@@ -70,32 +115,46 @@ int main(void)
 
     if((connect(user_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0)
     {
-        printf("Error while connecting\n");
+        char* error = "Error while connecting";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
 
-    if((key_len = send(user_fd, user_pk, sizeof(user_pk), 0)) < 0) // Sending user public key
+    log_message(log_file, "Connected to server", FALSE);
+
+    if((send(user_fd, user_pk, sizeof(user_pk), 0)) < 0) // Sending user public key
     {
-        perror("Couldn't send server public key\n");
+        char* error = "Couldn't send server public key\n";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
-    printf("%d\n", key_len);
-    printf("Send a key\n");
+
+    log_message(log_file, "Send user public key:", FALSE);
+    log_message(log_file, user_pk, FALSE);
 
     if((recv(user_fd, server_pk, sizeof(server_pk), 0)) <= 0) // Receiving server public key
     {
-        perror("Received empty key\n");
-        return -1;
-    }
-
-    if(crypto_scalarmult(shared_sk, user_sk, server_pk) != 0) // Making shared secret key
-    {
-        perror("Shared key error\n");
+        char* error = "Error receiving public key";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
+
+    log_message(log_file, "Received server public key:", FALSE);
+    log_message(log_file, server_pk, FALSE);
+
+    if(crypto_scalarmult(shared_sk, user_sk, server_pk) != 0) // Making shared secret key
+    {
+        char* error = "Shared key calculation error";
+        log_message(log_file, error, TRUE);
+        close(user_fd);
+        return -1;
+    }
+
+    log_message(log_file, "Calculated shared secret key:", FALSE);
+    log_message(log_file, shared_sk, FALSE);
 
     char* delim_ptr = strtok(ip_buf, ".");  //Getting server IP byte sequence
     while (delim_ptr != NULL)
@@ -111,15 +170,25 @@ int main(void)
 
     if((send(user_fd, MAC_CLIENT, sizeof(MAC_CLIENT), 0)) < 0) //Sending MAC_CLIENT
     {
+        char* error = "Couldn't send MAC_CLIENT";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
 
+    log_message(log_file, "Send MAC_CLIENT:", FALSE);
+    log_message(log_file, MAC_CLIENT, FALSE);
+
     if((recv(user_fd, MAC_SERVER, sizeof(MAC_SERVER), 0)) < 0) //Receiving MAC_SERVER
     {
+        char* error = "Couldn't receive MAC_SERVER";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
+
+    log_message(log_file, "Received MAC_SERVER:", FALSE);
+    log_message(log_file, MAC_SERVER, FALSE);
 
     memset(&ip_buf, 0, sizeof(ip_buf));
     memset(&ip_payload, 0 , sizeof(ip_payload));
@@ -141,23 +210,29 @@ int main(void)
 
     if ((crypto_auth_hmacsha256_verify(MAC_SERVER, ip_payload, sizeof(ip_payload), shared_sk)) < 0)
     {
-        perror("Verification error\n");
+        char* error = "Verification error";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
 
-    if((fp = fopen("CMakeLists.txt","r")) == NULL)
+    log_message(log_file, "Verification successful", FALSE);
+
+    if((read_fp = fopen(FILE_NAME,"r")) == NULL)
     {
-        perror("Couldn't open file\n");
+        char* error = "Couldn't open file";
+        log_message(log_file, error, TRUE);
         close(user_fd);
         return -1;
     }
 
-    randombytes_buf_deterministic(nonce, sizeof(nonce), MAC_SERVER);
+    log_message(log_file, "File opened", FALSE);
 
-    while(!feof(fp))
+    randombytes_buf_deterministic(nonce, sizeof(nonce), MAC_SERVER); //Generating seeded nonce
+
+    while(!feof(read_fp))
     {
-        if(fgets((char*)message_buf, sizeof(message_buf), fp))
+        if(fgets((char*)message_buf, sizeof(message_buf), read_fp))
         {
             crypto_aead_xchacha20poly1305_ietf_encrypt(cipher_msg, &cipher_len, message_buf,
                                                        sizeof(message_buf), NULL, 0, NULL,
@@ -167,8 +242,13 @@ int main(void)
         }
     }
 
-    fclose(fp);
+    log_message(log_file, "Message send", FALSE);
+
+    if(log_file)
+        fclose(log_file);
+    fclose(read_fp);
     close(user_fd);
 
     return 0;
 }
+
--- Server/main.c
+++ Server/main.c
@@ -11,8 +11,53 @@
 #define MAC_LENGTH 32
 #define SERVER_ADDRESS "127.0.0.1"
 #define MESSAGE_LENGTH 256
+#define TRUE 1
+#define FALSE 0
 
-int main(void) {
+void log_message(FILE *log_file, char *msg, int bool_error)
+{
+    if(!log_file)
+        printf("%s\n", msg);
+    else
+    {
+        fprintf(log_file, "%s", msg);
+        if(bool_error)
+            fclose(log_file);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if( argc < 2 || argc > 3 )
+    {
+        printf("Usage: ./Server <parameter>\n"
+               "-c Console logging\n"
+               "-f <log_file_name> File logging\n");
+        return -1;
+    }
+
+
+    if(sodium_init() < 0)
+    {
+        perror("No sodium\n");
+        return -1;
+    }
+
+    FILE* log_file = NULL;
+
+    if ( (strcmp(argv[1], "-f") == 0) && (argc == 3))
+    {
+        if ( (log_file = fopen(argv[2], "w")) == NULL)
+        {
+            perror("Couldn't create file\n");
+            return -1;
+        }
+    }
+    else if( (strcmp(argv[1], "-c") != 0) || (argc != 2))
+    {
+        printf("Wrong parameters. Check usage with ./Client\n");
+        return -1;
+    }
 
     struct sockaddr_in serv_addr, user_addr;
     int listen_fd = 0, conn_fd = 0, key_len = 0;
@@ -44,11 +89,13 @@ int main(void) {
 
     if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("Error while creating socket\n");
+        char* error = "Error while creating socket";
+        log_message(log_file, error, TRUE);
         close(listen_fd);
         return 0;
     }
 
+    log_message(log_file, "Socket created", FALSE);
 
     inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr);
     //serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -57,7 +104,8 @@ int main(void) {
 
     if((bind(listen_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) ) < 0)
     {
-        perror("Binding\n");
+        char* error = "Binding";
+        log_message(log_file, error, TRUE);
         close(listen_fd);
         return -1;
     }
@@ -68,50 +116,76 @@ int main(void) {
     {
         if((conn_fd = accept(listen_fd, (struct sockaddr*)&user_addr, &cli_len) ) < 0)
         {
-            perror("Error while accepting\n");
+            char* error = "Error while accepting";
+            log_message(log_file, error, TRUE);
             close(conn_fd);
             close(listen_fd);
             return -1;
         }
+
+        log_message(log_file, "User connected", FALSE);
 
         printf("Connected\n");
 
         if((key_len = recv(conn_fd, user_pk, sizeof(user_pk), 0)) <= 0) // Receiving user public key
         {
-            perror("Couldn't receive public key\n");
+            char* error = "Couldn't receive public key";
+            log_message(log_file, error, TRUE);
+            close(conn_fd);
+            close(listen_fd);
             break;
         }
 
+        log_message(log_file, "Received user public key:", FALSE);
+        log_message(log_file, user_pk, FALSE);
+
         if(key_len != crypto_box_PUBLICKEYBYTES)
         {
-            perror("Key wrong length\n");
+            char* error = "Key wrong length";
+            log_message(log_file, error, TRUE);
+            close(conn_fd);
+            close(listen_fd);
             return -1;
         }
-
-        printf("%d\n", key_len);
 
         crypto_box_keypair(server_pk, server_sk); // Forming public/secret key pair
 
         if((send(conn_fd, server_pk, sizeof(server_pk), 0)) < 0) // Sending server public key
         {
-            perror("Couldn't send server public key\n");
+            char* error = "Couldn't send server public key";
+            log_message(log_file, error, TRUE);
             close(conn_fd);
             close(listen_fd);
             return -1;
         }
+
+        log_message(log_file, "Send server public key:", FALSE);
+        log_message(log_file, server_pk, FALSE);
 
         if(crypto_scalarmult(shared_sk, server_sk, user_pk) != 0) // Making shared secret key
         {
-            perror("Shared key error\n");
+            char* error = "Shared key error";
+            log_message(log_file, error, TRUE);
             close(conn_fd);
             close(listen_fd);
             return -1;
         }
 
+        log_message(log_file, "Calculated shared secret key:", FALSE);
+        log_message(log_file, shared_sk, FALSE);
 
         inet_ntop(AF_INET, &serv_addr.sin_addr, (char*)ip_buf, sizeof(ip_buf)); //Get dotted-decimal format of server IP
 
-        recv(conn_fd, MAC_CLIENT, sizeof(MAC_CLIENT), 0);
+        if( (recv(conn_fd, MAC_CLIENT, sizeof(MAC_CLIENT), 0)) < 0) //Receiving MAC_CLIENT
+        {
+            char* error = "Couldn't receive MAC_CLIENT";
+            log_message(log_file, error, TRUE);
+            close(conn_fd);
+            return -1;
+        }
+
+        log_message(log_file, "Received MAC_CLIENT:", FALSE);
+        log_message(log_file, MAC_CLIENT, FALSE);
 
         ip_pay_ptr = ip_payload;
         char* delim_ptr = strtok(ip_buf, ".");  //Getting server IP byte sequence
@@ -126,11 +200,14 @@ int main(void) {
 
         if((crypto_auth_hmacsha256_verify(MAC_CLIENT, ip_payload, sizeof(ip_payload), shared_sk)) < 0)
         {
-            perror("Verification error\n");
+            char* error = "Verification error";
+            log_message(log_file, error, TRUE);
             close(conn_fd);
             close(listen_fd);
             return -1;
         }
+
+        log_message(log_file, "Verification successful", FALSE);
 
         memset(&ip_buf, 0, sizeof(ip_buf));
         memset(&ip_payload, 0 , sizeof(ip_payload));
@@ -150,23 +227,28 @@ int main(void) {
 
         crypto_auth_hmacsha256(MAC_SERVER, ip_payload, sizeof(ip_payload), shared_sk);
 
-        if((send(conn_fd, MAC_SERVER, sizeof(MAC_SERVER), 0) < 0))
+        if((send(conn_fd, MAC_SERVER, sizeof(MAC_SERVER), 0) < 0)) // Sending MAC_SERVER
         {
-            perror("Couldn't send MAC_SERVER\n");
+            char* error = "Couldn't send MAC_SERVER";
+            log_message(log_file, error, TRUE);
             close(conn_fd);
             close(listen_fd);
             return -1;
         }
 
+        log_message(log_file, "Send MAC_SERVER:", FALSE);
+        log_message(log_file, MAC_SERVER, FALSE);
+
         randombytes_buf_deterministic(nonce, sizeof(nonce), MAC_SERVER);
 
-        while((recv(conn_fd, cipher_msg, sizeof(cipher_msg), 0)) > 0)
+        while((recv(conn_fd, cipher_msg, sizeof(cipher_msg), 0)) > 0) //Receive encrypted message, decrypt and print it
         {
             if ((crypto_aead_xchacha20poly1305_ietf_decrypt(decrypted_msg, &decrypted_len, NULL,
                                                             cipher_msg, cipher_len,
                                                             NULL, 0, nonce, shared_sk)) < 0)
             {
-                perror("Decryption failed\n");
+                char* error = "Decryption failed";
+                log_message(log_file, error, TRUE);
                 close(conn_fd);
                 close(listen_fd);
                 return -1;
